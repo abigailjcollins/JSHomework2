ec0d4044aa8a10802d0e402922d9c144aeff4e71
Abigail Collins <abigailjcollins@gmail.com>
2024-01-25 19:18:32 -0500

    console log variable HW

---
+++ app.js
@@ -0,0 +1,35 @@
+var var1 = 1;
+var var2 = 2;
+var var3 = 3;
+var var4 = 4;
+var var5 = 5;
+var var6 = 6;
+var var7 = 7;
+var var8 = 8; 
+var var9 = 9;
+var var10 = 42;
+
+
+// Addition
+console.log(var1 + var8)
+console.log(var10 + var3)
+
+// Subtraction
+console.log(var8 - var2)
+console.log(var1 - var9)
+
+// Multiplication
+console.log(var10 * var10)
+console.log(var3 * var8)
+
+// Division
+console.log(var3 / var1)
+console.log(var10 / var4)
+
+// Increments
+console.log(++var2)
+console.log(++var6)
+
+// Decrements
+console.log(--var8)
+console.log(--var5)
